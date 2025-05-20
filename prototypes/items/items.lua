f651f70c9f4630b85a5af4f37c7136edcaa73bc1
The_ai <sans34678@gmail.com>
2025-05-20 00:16:30 -0400

    added gravel

--- prototypes/items/items.lua
+++ prototypes/items/items.lua
@@ -8,3 +8,14 @@ data:extend {{
     order = "aaa",
     stack_size = 50
 }}
+
+data:extend {{
+    type = "item",
+    name = "ore-gravel",
+    icon = "__gregtorio__/graphics/ores/ore-clay.png",
+    icon_size = 64,
+    flags = {},
+    subgroup = "gt-ores",
+    order = "aaa",
+    stack_size = 50
+}}
